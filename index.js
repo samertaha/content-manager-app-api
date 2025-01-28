c7361b68a4ee70408bbc319a9353711b79908bfa
Samer taha <alrazy1@gmail.com>
2025-01-28 13:08:21 +0200

    added post and patch api endpoints

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const resource = resources.find((resource) => resource.id === id);
+  res.send(resource);
+});
+
+app.patch("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const index = resources.findIndex((resource) => resource.id === id);
+
+  resources[index] = req.body;
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
+    if (err) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+    return res.send("Data has been updated!");
+  });
+});
+
 app.get("/api/resources", (req, res) => {
   const resources = getResources();
   res.send(resources);
@@ -25,10 +47,10 @@ app.post("/api/resources", (req, res) => {
   const resources = getResources();
   const resource = req.body;
 
-  resources.createdAt = new Date();
-  resources.status = "inactive";
-  resources.id = Date.now().toString();
-  resourses.unshift(resource);
+  resource.createdAt = new Date();
+  resource.status = "inactive";
+  resource.id = Date.now().toString();
+  resources.unshift(resource);
 
   fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (err) => {
     if (err) {
